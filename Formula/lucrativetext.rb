bbafd37a36763ee39bde12fcaf580c666f04d621
AK <aamaal00777@gmail.com>
2026-03-11 00:54:17 +0400

    lucrativetext link correction

--- Formula/lucrativetext.rb
+++ Formula/lucrativetext.rb
@@ -1,7 +1,7 @@
 class Lucrativetext < Formula
   desc "A lightweight terminal text editor"
-  homepage "https://github.com/amal-ak47/lucrativeText"
-  url "https://github.com/amal-ak47/lucrativeText/archive/refs/tags/v1.0.1.tar.gz"
+  homepage "https://github.com/amal-ak47/lucrativetext"
+  url "https://github.com/amal-ak47/lucrativetext/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "c49aa15a247b86dbfd340fff91612932f8027c30347f946852f878bbb040014c"
   license "CC-BY-NC-4.0"
 
